13bf30bbb237db8d6201f226b31aac84c6554c9b
Jeff Tilson <jrtilson@gmail.com>
2015-05-14 15:58:29 -0400

    Reworked to include a tab view controller. Added a help page with buttons to activate tutorial videos

---
+++ SpotConnect/HelpViewController.swift
@@ -0,0 +1,61 @@
+//
+//  HelpViewController.swift
+//  SpotConnect
+//
+//  Created by Jeff Tilson on 2015-05-14.
+//  Copyright (c) 2015 Jeff Tilson. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import Foundation
+import AVFoundation
+
+class HelpViewController: UIViewController {
+    // MARK: - Outlets
+    
+    @IBOutlet weak var signin: SpotHelpButton!
+    @IBOutlet weak var bookmark: SpotHelpButton!
+    @IBOutlet weak var photo: SpotHelpButton!
+    
+    // MARK: - UIViewController
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.twoColorLayerGradient(SpotColors.LIGHTRED, colorTwo: SpotColors.DARKRED)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Provide the corresponding video name in the button tag
+        signin.videoTitle = "signin_final"
+        signin.videoType = "mov"
+        
+        bookmark.videoTitle = "bookmark_final"
+        bookmark.videoType = "mov"
+        
+        photo.videoTitle = "photo_final"
+        photo.videoType = "mov"
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Make sure we were sent here with a help button
+        if let helpButton: SpotHelpButton = sender as? SpotHelpButton {
+            let destination = segue.destinationViewController as! AVPlayerViewController
+            
+            var url = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(helpButton.videoTitle, ofType: helpButton.videoType)!)
+
+            destination.player = AVPlayer(URL: url)
+            destination.videoGravity = AVLayerVideoGravityResizeAspectFill
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func helpButtonPressed(sender: SpotHelpButton) {
+        performSegueWithIdentifier("playVideo", sender: sender)
+    }
+  
+
+}
---
+++ SpotConnect/SpotHelpButton.swift
@@ -0,0 +1,15 @@
+//
+//  SpotHelpButton.swift
+//  SpotConnect
+//
+//  Created by Jeff Tilson on 2015-05-14.
+//  Copyright (c) 2015 Jeff Tilson. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class SpotHelpButton: Button {
+    var videoTitle: String?
+    var videoType: String?
+}
